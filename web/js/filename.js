6e6246e534eb9e7f415241c850d6faa31145b21f
Juhana Kaarlehto <juhana.kaarlehto@10bit.io>
2025-11-17 13:22:01 +0200

    Persist through refresh

--- web/js/filename.js
+++ web/js/filename.js
@@ -1,31 +1,77 @@
 import { app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 
+const workflowNameNodes = new Set();
+
 app.registerExtension({
     name: "workflow.getname",
     
-    async nodeCreated(node) {
-        console.log(app.extensionManager.workflow.activeWorkflow);
-        let filename = app.extensionManager.workflow.activeWorkflow.fullFilename;
-        let widget = node.widgets?.find(w => w.name === "workflow_name");
-        if (widget) {
-            widget.value = filename;
-        }
-
-        api.addEventListener('promptQueued', () => {
-            filename = app.extensionManager.workflow.activeWorkflow.fullFilename;
-            widget.value = filename;
-        });
+    async setup() {
+        patchWorkflowRename(app);
     },
-
+    
+    async afterConfigureGraph() {
+        workflowNameNodes.clear();
+        app.graph._nodes
+            ?.filter(n => n.comfyClass === "GetWorkflowName")
+            .forEach(n => workflowNameNodes.add(n));
+        
+        patchWorkflowRename(app);
+    },
+    
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeType.comfyClass == "GetWorkflowName") {
-            let filename = app.extensionManager.workflow.activeWorkflow.fullFilename;
-            let widget = node.widgets?.find(w => w.name === "workflow_name");
-            if (widget) {
-                widget.value = filename;
-            }
+        if (nodeData.name === "GetWorkflowName") {
+            const onNodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function() {
+                onNodeCreated?.apply(this, arguments);
+                
+                workflowNameNodes.add(this);
+                
+                patchWorkflowRename(app);
+                
+                const activeWorkflow = app.extensionManager?.workflow?.activeWorkflow;
+                let filename = activeWorkflow?.fullFilename?.replace(/\//g, '_') || "untitled";
+                let widget = this.widgets?.find(w => w.name === "workflow_name");
+                if (widget) {
+                    widget.value = filename;
+                }
+                
+                api.addEventListener('promptQueued', () => {
+                    filename = app.extensionManager.workflow.activeWorkflow?.fullFilename?.replace(/\//g, '_') || "untitled";
+                    widget.value = filename;
+                });
+            };
+            
+            const onRemoved = nodeType.prototype.onRemoved;
+            nodeType.prototype.onRemoved = function() {
+                onRemoved?.apply(this, arguments);
+                workflowNameNodes.delete(this);
+            };
         }
     }
-    
 });
+
+function patchWorkflowRename(app) {
+    const activeWorkflow = app.extensionManager?.workflow?.activeWorkflow;
+    
+    if (activeWorkflow && !activeWorkflow._renamePatched) {
+        const originalRename = activeWorkflow.rename.bind(activeWorkflow);
+        
+        activeWorkflow.rename = async function(newPath) {
+            const result = await originalRename(newPath);
+            
+            const newFilename = newPath.replace(/\//g, '_');
+            
+            workflowNameNodes.forEach(node => {
+                const w = node.widgets?.find(w => w.name === "workflow_name");
+                if (w) {
+                    w.value = newFilename;
+                }
+            });
+            
+            return result;
+        };
+        
+        activeWorkflow._renamePatched = true;
+    }
+}
